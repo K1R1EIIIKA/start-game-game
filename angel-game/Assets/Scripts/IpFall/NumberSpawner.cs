6b7ceac28fc98ae076dfb2d2d8641cabc45dbfc1
vladzabaluev <vladzabaluev014@gmail.com>
2023-10-01 22:05:51 +0300

    Microupdates

--- angel-game/Assets/Scripts/IpFall/NumberSpawner.cs
+++ angel-game/Assets/Scripts/IpFall/NumberSpawner.cs
@@ -2,28 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class NumberSpawner : MonoBehaviour
 {
     public GameObject number;
     public Transform parent;
     public GameObject canvas;
 
-     private List<GameObject> numberList = new List<GameObject>();
-   
-    public float minx, maxx, miny, maxy;
+    private List<GameObject> numberList = new List<GameObject>();
+
+    public float minX, maxX, minY, maxY;
 
     [SerializeField] private TextMeshProUGUI Score;
     private TextMeshProUGUI textNumber;
     public GameObject PanelLose;
-    // Start is called before the first frame update
-    void Start()
-    {
 
+    // Start is called before the first frame update
+    private void Start()
+    {
         StartCoroutine(ITimer());
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         Score.text = GlobalThings.countNumbers.ToString() + "/100";
         if (GlobalThings.countNumbers > 100)
@@ -31,23 +32,23 @@ public class NumberSpawner : MonoBehaviour
             canvas.SetActive(false);
         }
     }
+
     private void SpawnNumber()
     {
-
-        minx = -880;
-        maxx = 848;
-        miny = -40;
-        maxy = 446;
-        var wantedx = Random.Range(minx, maxx);
-        var wantedy = Random.Range(miny, maxy);
+        minX = -880;
+        maxX = 848;
+        minY = -40;
+        maxY = 446;
+        var wantedx = Random.Range(minX, maxX);
+        var wantedy = Random.Range(minY, maxY);
         var position = new Vector3(wantedx + 960, wantedy + 540);
         GameObject go = Instantiate(number, position, transform.rotation, parent);
         numberList.Add(go);
-
     }
+
     public void DeleteNumber()
     {
-        for (int i=0;i< numberList.Count; i++)
+        for (int i = 0; i < numberList.Count; i++)
         {
             Destroy(numberList[i]);
         }
@@ -56,7 +57,8 @@ public class NumberSpawner : MonoBehaviour
         PanelLose.SetActive(false);
         GlobalThings.ipGameisOn = true;
     }
-    IEnumerator ITimer()
+
+    private IEnumerator ITimer()
     {
         while (GlobalThings.ipGameisOn)
         {
