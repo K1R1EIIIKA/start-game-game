5d0d2a6b8e099973b95797bfea296aa6c2bd8229
vladzabaluev <vladzabaluev014@gmail.com>
2023-10-01 23:47:47 +0300

    Temp commit

---
+++ angel-game/Assets/Scripts/Task/ItemPorter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPorter : MonoBehaviour
+{
+    private bool _isBusy;
+
+    [SerializeField] private Transform _itemSpot;
+    private Item _currentItem;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public void TryJumpInBag(Item item)
+    {
+        if (!_isBusy)
+        {
+            _currentItem = item;
+
+            _currentItem.transform.position = _itemSpot.position;
+            _currentItem.transform.SetParent(_itemSpot);
+            _isBusy = true;
+        }
+    }
+
+    public void JumpOutBag(Item item)
+    {
+        _isBusy = false;
+    }
+}
